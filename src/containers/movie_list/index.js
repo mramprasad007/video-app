fea2771695e08d607ba3eb395cd886562584749d
mramprasad007 <mramprasad007@gmail.com>
2018-09-15 13:21:37 +0530

    page 1 integrated

--- src/containers/movie_list/index.js
+++ src/containers/movie_list/index.js
@@ -14,15 +14,15 @@ class MovieList extends Component {
 	}
 
 	render() {
-		console.log(this.props.movieList);
+		console.log(this.props.movieList.pages);
 		return (
 			<div>
-				<div className="header flex items-center shadow-md fixed">
+				<div className="header flex items-center shadow-md fixed pin-t">
 					<div className="back-button">
 						<img src="assets/Back.png" />
 					</div>
 					<div className="page-title">
-						{this.props.movieList.pages.title
+						{this.props.movieList.pages
 							? this.props.movieList.pages.title
 							: ''}
 					</div>
@@ -31,7 +31,36 @@ class MovieList extends Component {
 						<img src="assets/search.png" />
 					</div>
 				</div>
-				<div />
+				<div className="content flex flex-wrap">
+					{this.props.movieList.pages['content-items']
+						? this.props.movieList.pages[
+								'content-items'
+						  ].content.map((item, index) => (
+								<div
+									className="movie-card w-1/3"
+									key={
+										this.props.movieList.pages[
+											'page-num-requested'
+										] +
+										'' +
+										index
+									}
+								>
+									<div className="movie-img">
+										<img
+											className="movie-img-src"
+											src={
+												'assets/' + item['poster-image']
+											}
+										/>
+									</div>
+									<div className="movie-text">
+										{item.name}
+									</div>
+								</div>
+						  ))
+						: null}
+				</div>
 			</div>
 		);
 	}
